9ae37719a05c16845c9444fbc4f94b18c668c570
Ahmed ramadan <askme557@gmail.com>
2023-03-20 18:20:19 -0700

    0x01. C task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+	int number;
+
+	for (number = '0'; number <= '9'; number++)
+		putchar(number);
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
