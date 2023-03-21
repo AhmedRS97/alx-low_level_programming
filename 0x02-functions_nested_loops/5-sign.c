f50a3c025e4e8777eca4e495f41cb2f9d3960620
Ahmed ramadan <askme557@gmail.com>
2023-03-21 05:01:37 -0700

    0x02. C - task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - print the sign of integer n
+ * @n: an integer
+ *
+ * Return: Always int.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
+
