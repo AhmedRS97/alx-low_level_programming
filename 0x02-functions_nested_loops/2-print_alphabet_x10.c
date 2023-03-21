c2132914232dd28fa834c62044c027c9a2c87106
Ahmed ramadan <askme557@gmail.com>
2023-03-21 04:05:24 -0700

    0x02. C - task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet ten times
+ *
+ * Return: void.
+ */
+void print_alphabet_x10(void)
+{
+	for (int i = 0; i <= 9; i++)
+		print_alphabet();
+}
+
