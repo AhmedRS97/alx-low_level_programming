84699c9d87c815629dbfd4fea55d2aaae7de55e0
Ahmed ramadan <askme557@gmail.com>
2023-03-22 16:15:20 -0700

    0x02. C - task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Add two numbers
+ * @first_num: an integer
+ * @second_num: an integer
+ *
+ * Return: Always int.
+ */
+int add(int first_num, int second_num)
+{
+	return (first_num + second_num);
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,6 +7,7 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
+int add(int, int);
 void jack_bauer(void);
 void times_table(void);
 void print_alphabet(void);
