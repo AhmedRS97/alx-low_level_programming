8d0e06934516d2e635a433165f10437a98cf440e
Ahmed ramadan <askme557@gmail.com>
2023-03-24 07:18:36 -0700

    0x04. C - task 8 updated

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,7 +2,7 @@
 
 /**
  * print_square - print a square to the terminal
- * @n: An integer number
+ * @size: An integer number
  *
  * Return: void.
  */
@@ -10,11 +10,11 @@ void print_square(int size)
 {
 	int i, j;
 
-	if (n <= 0)
+	if (size <= 0)
 		_putchar('\n');
-	for (i = 0; i < n; i++)
+	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < n; j++)
+		for (j = 0; j < size; j++)
 			_putchar('#');
 		_putchar('\n');
 	}
