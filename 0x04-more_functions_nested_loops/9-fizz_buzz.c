1176bde367003c430d47acbf060556e8e9b56dcf
Ahmed ramadan <askme557@gmail.com>
2023-03-24 07:40:36 -0700

    0x04. C - task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * print_fizz_buzz - print fizz buzz test to the terminal
+ *
+ * Return: void.
+ */
+void print_fizz_buzz(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
+	}
+}
+
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_fizz_buzz();
+	return (0);
+}
+
